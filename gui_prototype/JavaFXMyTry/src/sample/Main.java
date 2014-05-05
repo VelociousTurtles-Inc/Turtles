4e88e543c2df4b4cabf14e9aa0d863ddef6678ab
MuchomorGrabarz <mz18@o2.pl>
2014-05-05 12:56:42 +0200

    Co to się podziało

---
+++ gui_prototype/JavaFXMyTry/src/sample/Main.java
@@ -0,0 +1,50 @@
+package sample;
+
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.stage.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws InterruptedException {
+        primaryStage.setTitle("JavaFX Welcome");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(30, 60, 30, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 1, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        primaryStage.setScene(new Scene(grid, 300, 150));
+
+        primaryStage.show();
+        primaryStage.setResizable(false);
+
+        Thread.currentThread().sleep(2000);
+        primaryStage.getScene().getRoot().requestLayout();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
