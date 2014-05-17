4ebd25bb107c39536dbdd244f73b2048348cba51
Larhard <larhard@gmail.com>
2014-05-17 15:27:34 +0200

    working Images.load()

---
+++ Tests/src/Images/ImagesTest.java
@@ -0,0 +1,16 @@
+package Images;
+
+import javafx.scene.image.Image;
+import junit.framework.TestCase;
+
+public class ImagesTest extends TestCase {
+
+    public void testLoad() throws Exception {
+        Image image = Images.load(this.getClass().getClassLoader(), "Images/TestResources/TestImage.png");
+        assertEquals(170.0, image.getWidth());
+        image = Images.load(this.getClass().getClassLoader(), "Images/TestResources/TestImage.png", 200, 100, false, false);
+        assertEquals(200.0, image.getWidth());
+        image = Images.load(this.getClass().getClassLoader(), "Images/TestResources/TestImage.png", 200, 100, true, false);
+        assertEquals(100.0, image.getWidth());
+    }
+}
--- Tests/src/Tests/RunAllTests.java
+++ Tests/src/Tests/RunAllTests.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import Images.ImagesTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -7,9 +8,13 @@ import junit.framework.TestSuite;
  * Created by larhard on 07.05.14.
  */
 public class RunAllTests extends TestSuite {
+
+    /**
+     * to add new TestCase simply suite.addTestSuite(TestCase.class);
+     */
     public static Test suite() {
         final TestSuite suite = new TestSuite();
-        //suite.addTestSuite(Test.class);
+        suite.addTestSuite(Images.ImagesTest.class);
         return suite;
     }
 }
