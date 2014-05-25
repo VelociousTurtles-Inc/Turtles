54413c98afedc63cf2d0350e8c751b9034c16c04
Larhard <larhard@gmail.com>
2014-05-26 00:15:34 +0200

    Server adapted to screen environment

--- Server/src/Main/Server.java
+++ Server/src/Main/Server.java
@@ -1,11 +1,13 @@
 package Main;
 
 import GameDispenser.StandardGameDispenser;
+import Model.Utility.Utility;
 import Scenarios.Scenario;
 import Scenarios.StandardScenario;
 import Server.Interfaces.ServerGameDispenser;
 import org.cojen.dirmi.Environment;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -17,22 +19,36 @@ public class Server {
     public static void main(String[] args) throws Exception {
         scenario = new StandardScenario();
         port = args.length > 1 ? Integer.valueOf(args[0]) : 8080;
-        System.out.println("Server starting...");
-        Environment environment = new Environment();
-        ServerGameDispenser gameDispenser = new StandardGameDispenser();
-        environment.newSessionAcceptor(port).acceptAll(gameDispenser);
-        System.out.println("Server started");
-        System.out.println("type exit to stop server");
-        Scanner scanner = new Scanner(System.in);
-        for(;;) {
-            String command = scanner.nextLine();
-            if (command.equals("exit")) {
-                break;
+        Environment environment = null;
+        ServerGameDispenser  gameDispenser = null;
+        try {
+            System.out.println("Server starting...");
+            environment = new Environment();
+            gameDispenser = new StandardGameDispenser();
+            environment.newSessionAcceptor(port).acceptAll(gameDispenser);
+            System.out.println("Server started");
+            System.out.println("type exit to stop server");
+            Scanner scanner = new Scanner(System.in);
+            for (; ; ) {
+                try {
+                    String command = scanner.nextLine();
+                    if (command.equals("exit")) {
+                        break;
+                    }
+                } catch (NoSuchElementException e) {
+                    Utility.logInfo("Assumed there is no input stream connected");
+                    Thread.sleep(86400000);
+                }
             }
+        } finally {
+            System.out.println("Server stopping...");
+            if (gameDispenser != null) {
+                gameDispenser.close();
+            }
+            if (environment != null) {
+                environment.close();
+            }
+            System.out.println("Server stopped");
         }
-        System.out.println("Server stopping...");
-        gameDispenser.close();
-        environment.close();
-        System.out.println("Server stopped");
     }
 }
