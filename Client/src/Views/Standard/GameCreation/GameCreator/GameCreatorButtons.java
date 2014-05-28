012f7aedf5495251b518754ec01c9ddb1b2a8e40
Larhard <larhard@gmail.com>
2014-05-29 00:47:05 +0200

    GameCreator View upgraded

--- Client/src/Views/Standard/GameCreation/GameCreator/GameCreatorButtons.java
+++ Client/src/Views/Standard/GameCreation/GameCreator/GameCreatorButtons.java
@@ -2,8 +2,13 @@ package Views.Standard.GameCreation.GameCreator;
 
 import Common.Interfaces.Event;
 import Controllers.Interfaces.GameCreatorController;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.rmi.RemoteException;
@@ -13,6 +18,7 @@ public class GameCreatorButtons {
     public TextField name;
     Stage myStage;
     GameCreatorController myController;
+    public Button createButton;
 
     public void setStage(Stage stage) {
         myStage = stage;
@@ -25,6 +31,22 @@ public class GameCreatorButtons {
                 myStage.close();
             }
         });
+
+        name.addEventFilter(KeyEvent.ANY, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    if(keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
+                        try {
+                            create(null);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    keyEvent.consume();
+                }
+            }
+        });
     }
 
     public void cancel(ActionEvent actionEvent) {
@@ -32,6 +54,15 @@ public class GameCreatorButtons {
     }
 
     public void create(ActionEvent actionEvent) throws RemoteException {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    createButton.setDisable(true);
+                    createButton.setText("Creating...");
+                } catch (NullPointerException ignored) {}
+            }
+        });
         myController.create(String.valueOf(name.getCharacters()));
         cancel(actionEvent);
     }
